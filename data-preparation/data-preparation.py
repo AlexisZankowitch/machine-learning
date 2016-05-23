d1219dca72b16e064186b6a1cb1aa27dfa24c7bd
Alexis Zankowitch <zankowitch@gmail.com>
2016-05-23 23:41:19 +0200

    reset

--- data-preparation/data-preparation.py
+++ data-preparation/data-preparation.py
@@ -2,3 +2,9 @@ import pandas as pd
 
 data = pd.read_csv('../data/DataSet.csv')
 
+continuousFeaturesList = ['age', 'fnlwgt', 'education-num', 'capital-gain', 'capital-loss', 'hours-per-week']
+
+continuousFeature = dict()
+continuousFeatureKey = ['Count', '% Miss', 'Card', 'min', '1st Qrt', 'Mean', 'Median', '3rd Qrt', 'Max', 'Std Dev']
+
+# for key in continuousFeaturesList:
