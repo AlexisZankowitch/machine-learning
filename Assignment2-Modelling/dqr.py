cdd211949a45d5caa61371db05dab7748b23892f
Alexis Zankowitch <zankowitch@gmail.com>
2016-05-25 17:46:57 +0200

    change data

--- Assignment2-Modelling/dqr.py
+++ Assignment2-Modelling/dqr.py
@@ -1,10 +1,30 @@
 import pandas as pd
 
-data = pd.read_csv('../data/bank/DataSet.csv').select_dtypes(include=[object])
+data = pd.read_csv('../data/bank/bank-full.csv').select_dtypes(include=[object])
 dData = data.describe().transpose()
-dData = dData.drop('id')
-dData = dData.drop('target')
+dData = dData.drop('y')
+dData = data.describe().transpose()
 
+# CONTINUOUS FEATURES
+continuousFeaturesList = ['age', 'balance', 'day', 'duration', 'campaign', 'pdays', 'previous']
+
+# add columns
+dData['Miss %'] = ""
+dData['Card'] = ""
+
+for i in range(0, len(continuousFeaturesList)):
+    # Miss % calculation
+    myList = data[continuousFeaturesList[i]].tolist()
+    dData.iat[i, 8] = myList.count('N/A')
+    # Cardinality calculation
+    dData.iat[i, 9] = len(data[continuousFeaturesList[i]].unique())
+
+df = pd.DataFrame(dData, columns=['count', 'Miss %', 'Card', 'min', '25%', 'mean', '50%', '75%', 'max', 'std'])
+df.columns = ['Count', 'Miss %', 'Card', 'Min', '1st Qrt', 'Mean', 'Median', '3rd Qrt', 'Max', 'Std']
+# storage
+df.to_csv('../data/bank/gouhier-zankowitch-DQR-ContinuousFeatures.csv')
+
+# CATEGORICAL FEATURES
 categoricalFeaturesList = ['workclass', 'education', 'marital-status', 'occupation', 'relationship', 'race', 'sex',
                            'native-country']
 dData['Mode %'] = None
@@ -23,5 +43,5 @@ for i in range(0, len(categoricalFeaturesList)):
 
 df = pd.DataFrame(dData, columns=['count', 'Miss', 'unique', 'top', 'freq', 'Mode %', 'Mode2', 'Mode2 freq', 'Mode2 %'])
 df.columns = ['Count', 'Miss %', 'Card', 'Mode', 'Mode freq', 'Mode %', '2nd Mode', '2nd Mode Freq', '2nd Mode %']
-#storage
+# storage
 df.to_csv('../data/boucher-zankowitch-DQR-CategoricalFeatures.csv')
